7943d0c778ec961e0f456a0b209e570b8881a66c
hanifanm <hanifanmohamad@gmail.com>
2018-10-08 22:46:59 +0700

    save all renaming

---
+++ p5/3-inverse-kinematics/sketch.js
@@ -0,0 +1,27 @@
+
+let target;
+let tentacles = [];
+
+function setup() {
+	createCanvas(600, 400);
+	target = new Circle(width/2, height/2, 50, PI/5, 4);
+	tentacles.push(new Tentacle(width/2, 0, 6));
+	tentacles.push(new Tentacle(0, height, 5));
+	tentacles.push(new Tentacle(width, height, 5));
+}
+
+function mousePressed() {
+	target.x = mouseX;
+	target.y = mouseY;
+}
+
+function draw() {
+	background(0, 0, 255);
+
+	target.update();
+	target.show();
+	for(let i=0; i<tentacles.length; i++) {
+		tentacles[i].show(target.x, target.y);
+	}
+	
+}
