ac46e1500752c04b8289a74c99f7d09f2c1c3ad6
hanifanm <hanifanmohamad@gmail.com>
2019-05-30 17:15:00 +0700

    fix image path

--- web-remake/nationalgeographic/component/navbar.js
+++ web-remake/nationalgeographic/component/navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
   }
 
   return <header style={headerStyle} className="d-flex align-items-center px-3 ng-navbar">
-    <a href><img src='/assets/images/ng-logo-2fl.svg' height='32px' /></a>
+    <a href><img src='assets/images/ng-logo-2fl.svg' height='32px' /></a>
     <ul className="d-flex align-items-center ml-auto ng-navbar-menu m-0 p-0">
       <li className="d-none d-md-block mr-3 c-pointer ng-navbar-subscribe">Subscribe</li>
       <li className="d-block mr-3 c-pointer"><span className="far fa-user"></span></li>
