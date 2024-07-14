fe5de27f0b7a1a200d3b5ed6d9bbb5291cdb8d56
Mohamad Hanifan <mohamad.hanifan@efishery.com>
2024-07-14 22:36:13 +0700

    feat: add markdown

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
 import styles from './page.module.css'
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+import { getMarkdownContent } from '@/utils/read-file'
+
+const markdown = `Just a link: https://reactjs.com.`
 
 export default function Home() {
-  return <main className={styles.main}>Hello, World!</main>
+  const contents = getMarkdownContent('md/profile.md')
+  return (
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <ReactMarkdown children={contents} remarkPlugins={[remarkGfm]} />
+      </div>
+    </main>
+  )
 }
