b20ce82b3713946366e38735813764a57a6eca5f
Mohamad Hanifan <mohamad.hanifan@efishery.com>
2024-07-14 22:40:32 +0700

    feat: fix issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,12 @@ import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 import { getMarkdownContent } from '@/utils/read-file'
 
-const markdown = `Just a link: https://reactjs.com.`
-
 export default function Home() {
   const contents = getMarkdownContent('md/profile.md')
   return (
     <main className={styles.main}>
       <div className={styles.container}>
-        <ReactMarkdown children={contents} remarkPlugins={[remarkGfm]} />
+        <ReactMarkdown remarkPlugins={[remarkGfm]}>{contents}</ReactMarkdown>
       </div>
     </main>
   )
