980bc09a6fb8b8ace1f7daedcc4edf7f621acaab
Mohamad Hanifan <mohamad.hanifan@efishery.com>
2024-07-15 08:38:11 +0700

    feat: fix padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
+import cx from 'classnames'
 
 import styles from './page.module.css'
 
@@ -8,12 +9,8 @@ import { getMarkdownContent } from '@/utils/read-file'
 export default function Home() {
   const contents = getMarkdownContent('md/profile.md')
   return (
-    <main className={styles.main}>
-      <div className={styles.container}>
-        <div className='markdown-body'>
-          <ReactMarkdown remarkPlugins={[remarkGfm]}>{contents}</ReactMarkdown>
-        </div>
-      </div>
+    <main className={cx(styles.main, 'markdown-body')}>
+      <ReactMarkdown remarkPlugins={[remarkGfm]}>{contents}</ReactMarkdown>
     </main>
   )
 }
