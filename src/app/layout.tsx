e8f8ee4c5aec3a2e933f05573fc9c9c1d68d6749
Mohamad Hanifan <mohamad.hanifan@efishery.com>
2024-07-20 04:13:57 +0700

    feat: fix container

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className='main-container'>{children}</main>
+      </body>
     </html>
   )
 }
