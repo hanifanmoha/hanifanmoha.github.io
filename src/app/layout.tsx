218fe9a99b80573e0e6f2e7c8b8c8d26ded45fd7
Mohamad Hanifan <mohamad.hanifan@efishery.com>
2024-07-28 19:08:39 +0700

    feat: add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 
 import './globals.css'
 import 'github-markdown-css/github-markdown-dark.css'
+import GoogleAnalytics from '@/components/GoogleAnalytics'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>
+        <GoogleAnalytics />
         <main className='main-container'>{children}</main>
       </body>
     </html>
