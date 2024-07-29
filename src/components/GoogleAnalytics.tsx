b58dcd57604e7dc8fa54411f52379e3d0c6d2803
Mohamad Hanifan <mohamad.hanifan@efishery.com>
2024-07-29 08:22:01 +0700

    feat: move measuerment id to config

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -2,13 +2,14 @@
 
 import React from 'react'
 import Script from 'next/script'
+import CONFIG from '@/utils/config'
 
 const GoogleAnalytics = () => {
   return (
     <>
       <Script
         strategy='lazyOnload'
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${CONFIG.GA_MEASUREMENT_ID}`}
       />
 
       <Script id='' strategy='lazyOnload'>
@@ -16,7 +17,7 @@ const GoogleAnalytics = () => {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}', {
+              gtag('config', '${CONFIG.GA_MEASUREMENT_ID}', {
               page_path: window.location.pathname,
               });
           `}
---
+++ src/utils/config.ts
@@ -0,0 +1,5 @@
+const CONFIG = {
+  GA_MEASUREMENT_ID: 'G-KNJ2LBND29',
+}
+
+export default CONFIG
